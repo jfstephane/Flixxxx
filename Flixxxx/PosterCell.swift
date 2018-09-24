6f9851d1866a133421b316fd17f0c5b5b83f354a
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2018-09-24 17:21:03 -0400

    collectionView
    CollectionView ok

---
+++ Flixxxx/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flixxxx
+//
+//  Created by Jules Frantz Stephane Loubeau on 9/24/18.
+//  Copyright © 2018 Jules Frantz Stephane Loubeau. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImage: UIImageView!
+}
